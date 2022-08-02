576dea3666ac8da14a3a1e266b87795d99b9b215
Igor Buzykin <lexemz@gmail.com>
2022-08-02 15:09:37 +0300

    Project preparation

---
+++ SwinjectTest/SecondViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SecondViewController.swift
+//  SwinjectTest
+//
+//  Created by Igor Buzykin on 02.08.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+}
--- SwinjectTest/ViewController.swift
+++ SwinjectTest/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func goButtonPressed(_ sender: Any) {
+        let viewController = storyboard?.instantiateViewController(withIdentifier: "SecondViewController")
+        guard let secondVC = viewController as? SecondViewController else { return }
+        
+        present(secondVC, animated: true)
+    }
 }
 
